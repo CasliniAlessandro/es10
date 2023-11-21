467dc698ccfaa27357ad6721fd7a68447573c70a
caslini.20353 <caslini.20353@E2-9-PC20>
2023-11-21 12:32:26 +0100

    ..

--- es10/articolo.cs
+++ es10/articolo.cs
@@ -6,10 +6,51 @@ using System.Threading.Tasks;
 
 namespace es10
 {
-    internal class articolo
+    class articolo
     {
+        public string Codice { get; set; }  
+        public string Descrizione { get; set; }
+        public decimal PrezzoUnitario { get; set; } 
+        public bool TesseraFedelta { get; set; }    
 
-
-
+        public articolo(string codice, string descrizione, decimal prezzoUnitario)
+        {
+            Codice = codice;
+            Descrizione = descrizione;
+            PrezzoUnitario = prezzoUnitario;
+        } 
+        
+        public virtual double Sconta()
+        {
+            if (TesseraFedelta) 
+            {
+                PrezzoUnitario = PrezzoUnitario- PrezzoUnitario*(5/100);
+                
+            }
+            return PrezzoUnitario;
+        }
     } 
+
+    class ArticoloAlimentare : articolo
+    {
+        public int AnnoScadenza { get; set; }   
+        public ArticoloAlimentare(string codice, string descrizione, decimal prezzoUnitario,int annoScadenza) : base(codice,descrizione,prezzoUnitario)
+        {
+            AnnoScadenza = annoScadenza;
+        }
+
+        public override void Sconta()
+        {
+            base.Sconta();  
+        }
+    }
+
+    class ArticoloAlimentareFresco : ArticoloAlimentare
+    {
+        public int GiorniDopoApertura { get; set; }
+        public ArticoloAlimentareFresco(string codice, string descrizione, decimal prezzoUnitario, int annoScadenza,int GiorniDopoApertura): base (codice, descrizione, prezzoUnitario)
+        {
+
+        }
+    }
 }
